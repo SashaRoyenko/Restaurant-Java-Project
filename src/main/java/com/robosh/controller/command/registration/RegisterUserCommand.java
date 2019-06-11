65034f0109d4cd6dd82b11f6ae48b339d2fbc1d0
SashaRoyenko <sasharoyenko@gmail.com>
2019-06-11 17:01:00 +0300

    Finish registration

--- src/main/java/com/robosh/controller/command/registration/RegisterUserCommand.java
+++ src/main/java/com/robosh/controller/command/registration/RegisterUserCommand.java
@@ -2,6 +2,10 @@ package com.robosh.controller.command.registration;
 
 import com.robosh.controller.command.Command;
 import com.robosh.controller.command.login.LoginUserCommand;
+import com.robosh.controller.utils.InputDataUtil;
+import com.robosh.controller.utils.PagesPath;
+import com.robosh.controller.utils.SessionUtil;
+import com.robosh.model.connection.ConnectionPoolHolder;
 import com.robosh.model.entity.User;
 import com.robosh.model.exceptions.EmailIsAlreadyTaken;
 import com.robosh.model.exceptions.PhoneIsAlreadyTaken;
@@ -24,32 +28,55 @@ public class RegisterUserCommand implements Command {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        final String firstName = request.getParameter("firstName");
-        final String lastName = request.getParameter("lastName");
-        final String phone = request.getParameter("phone");
-        final String email = request.getParameter("email");
-        final String password = request.getParameter("password");
-        final String confirmedPassword = request.getParameter("confirmedPassword");
+        final String firstName = InputDataUtil.removeSpaces(request.getParameter("firstName"));
+        final String lastName = InputDataUtil.removeSpaces(request.getParameter("lastName"));
+        final String phone = InputDataUtil.removeSpaces(request.getParameter("phone"));
+        final String email = InputDataUtil.removeSpaces(request.getParameter("email"));
+        final String password = InputDataUtil.removeSpaces(request.getParameter("password"));
+        final String confirmedPassword = InputDataUtil.removeSpaces(request.getParameter("confirmedPassword"));
 
         if (password.equals(confirmedPassword)) {
-
-            User user = User.newBuilder()
-                    .setFirstName(firstName)
-                    .setLastName(lastName)
-                    .setEmail(email)
-                    .setPassword(password)
-                    .setPhone(phone)
-                    .build();
-            System.out.println(user);
-            try {
-                userService.createUser(user);
-                orderProductsService.createOrderProductsWithUSer(user);
-            } catch (EmailIsAlreadyTaken | PhoneIsAlreadyTaken emailIsAlreadyTaken) {
-                emailIsAlreadyTaken.printStackTrace();
-                new RegistrationPageCommand().execute(request, response);
+            if(!InputDataUtil.isCorrectName(firstName) || !InputDataUtil.isCorrectName(lastName)){
+                request.getRequestDispatcher(PagesPath.REGISTRATION_PAGE + "?correctName=false").forward(request, response);
+                return;
             }
+
+            if(!InputDataUtil.isCorrectPhone(phone)){
+                request.getRequestDispatcher(PagesPath.REGISTRATION_PAGE + "?correctPhone=false").forward(request, response);
+                return;
+            }
+
+            if(!InputDataUtil.isCorrectEmail(email)){
+                request.getRequestDispatcher(PagesPath.REGISTRATION_PAGE + "?correctEmail=false").forward(request, response);
+                return;
+            }
+
+            if(!InputDataUtil.isCorrectPassword(password)){
+                request.getRequestDispatcher(PagesPath.REGISTRATION_PAGE + "?correctPassword=false").forward(request, response);
+                return;
+            }
+
+            try {
+                User user = User.newBuilder()
+                        .setFirstName(firstName)
+                        .setLastName(lastName)
+                        .setEmail(email)
+                        .setPassword(password)
+                        .setPhone(phone)
+                        .build();
+                userService.createUser(user);
+                user = userService.getUserByEmailAndPassword(email, password);
+                SessionUtil.storeUserInSession(request.getSession(), user);
+                new LoginUserCommand(userService).execute(request, response);
+            } catch (EmailIsAlreadyTaken emailIsAlreadyTaken) {
+                request.getRequestDispatcher(PagesPath.REGISTRATION_PAGE + "?emailExist=true").forward(request, response);
+            } catch (PhoneIsAlreadyTaken phoneIsAlreadyTaken) {
+                request.getRequestDispatcher(PagesPath.REGISTRATION_PAGE + "?phoneExist=true").forward(request, response);
+            }
+        } else {
+            request.getRequestDispatcher(PagesPath.REGISTRATION_PAGE + "?passwordMatch=false").forward(request, response);
         }
-        new LoginUserCommand(userService).execute(request, response);
+
     }
 
 }
